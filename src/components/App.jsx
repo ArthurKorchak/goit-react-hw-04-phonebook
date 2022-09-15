72604dace9d9c91608cad00898e16491770a8966
ArthurKorchak <korchak.arthur@gmail.com>
2022-09-16 01:20:25 +0300

    Completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,59 +1,58 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid'
 import { InputForm } from './InputForm/InputForm';
 import { ContactsList } from './ContactsList/ContactsList';
 import { Filter } from './Filter/Filter';
 import s from './App.module.css';
 
-class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
+function App() {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
+  const [mountMarker, setMountMarker] = useState(true);
 
-  componentDidMount() {
-    const currentLS = localStorage.getItem('contactsList')
+  useEffect(() => {
+    const currentLS = localStorage.getItem('contactsList');
     if (currentLS) {
-      this.setState({contacts: JSON.parse(currentLS)});
-    }
-  };
-
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.contacts !== this.state.contacts) {
-      localStorage.setItem('contactsList', JSON.stringify(this.state.contacts));
+      setContacts(JSON.parse(currentLS));
     };
-  };
+  }, []);
+  
+  useEffect(() => {
+    if (mountMarker) {
+      setMountMarker(false);
+      return;
+    };
+    localStorage.setItem('contactsList', JSON.stringify(contacts));
+  }, [contacts, mountMarker]);
 
-  addContact = (name, number) => {
-    if (this.state.contacts.reduce((acc, item) => [...acc, item.name], []).includes(name)) {
+  const addContact = (name, number) => {
+    if (contacts.reduce((acc, item) => [...acc, item.name], []).includes(name)) {
       alert(`${name} is already in contacts`);
     } else {
-      const currentContacts = [...this.state.contacts, { id: nanoid(), name: name, number: number }]
-      this.setState({contacts: currentContacts});
+      const currentContacts = [...contacts, { id: nanoid(), name: name, number: number }];
+      setContacts(currentContacts);
     };
   };
 
-  deleteContact = (event) => {
-    const currentContacts = [...this.state.contacts].filter(item => item.id !== event.target.name);
-    this.setState({contacts: currentContacts});
+  const deleteContact = (event) => {
+    const currentContacts = [...contacts].filter(item => item.id !== event.target.name);
+    setContacts(currentContacts);
   };
 
-  filterOperator = (event) => {
-    this.setState({filter: event.target.value.toLowerCase()});
+  const filterOperator = (event) => {
+    setFilter(event.target.value.toLowerCase());
   };
 
-  render() {
-    const currentContacts = this.state.contacts.filter(item => item.name.toLowerCase().includes(this.state.filter))
-    return (
-      <div className={s.container}>
-        <h1>Phonebook</h1>
-        <InputForm addContact={this.addContact} />
-        <h2>Contacts</h2>
-        <Filter filter={this.state.filter} filterOperator={this.filterOperator}/>
-        <ContactsList currentContacts={currentContacts} deleteContact={this.deleteContact} />
-      </div>
-    );
-  };
+  const currentContacts = contacts.filter(item => item.name.toLowerCase().includes(filter))
+  return (
+    <div className={s.container}>
+      <h1>Phonebook</h1>
+      <InputForm addContact={addContact} />
+      <h2>Contacts</h2>
+      <Filter filter={filter} filterOperator={filterOperator}/>
+      <ContactsList currentContacts={currentContacts} deleteContact={deleteContact} />
+    </div>
+  );
 };
 
 export default App;
--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,25 +1,22 @@
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 import s from './Filter.module.css';
 
-export class Filter extends Component {
-    render() {
-        return (
-            <label className={s.label}>
-                Find contacts by name
-                <input
-                    type="text"
-                    name="filter"
-                    pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-                    title="Name may contain only letters, apostrophe, dash and spaces. 
+export function Filter({ filter, filterOperator }) {
+    return (
+        <label className={s.label}>
+            Find contacts by name
+            <input
+                type="text"
+                name="filter"
+                pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                title="Name may contain only letters, apostrophe, dash and spaces. 
                     For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-                    required
-                    value={this.props.filter}
-                    onChange={this.props.filterOperator}
-                />
-            </label>
-        );
-    };
+                required
+                value={filter}
+                onChange={filterOperator}
+            />
+        </label>
+    );
 };
 
 Filter.propTypes = {
